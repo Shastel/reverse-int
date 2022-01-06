0a19b5f3956353983d9cf64d586ad77c282f440e
Marianchuk <marianchuksv@gmail.com>
2022-01-06 14:01:29 +0200

    add: js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+   return Array.from(n.toString()).reverse().join('')
+    } else if (n < 0) {
+        return  (Array.from(n.toString()).reverse().join('')).slice(0, -1)
+    }
 }
