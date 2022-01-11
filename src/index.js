4e3566f0c5d87cd9387e14e3faf612a8dc006758
mahotich <rmahotka@gmail.com>
2022-01-11 14:25:54 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reversChar = String(n).split("").reverse().join("");
+    if (n > 0) {
+        return reversChar;
+    }
+    else {
+        return reversChar.substring(0, reversChar.length - 1);
+    }
 }
