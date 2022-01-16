bc8ea51a5fee60c2570d63810d72ca850c8d7656
Valery <vteramin@gmail.com>
2022-01-16 03:38:27 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let str = Math.abs(n).toString();
+    let l = str.length;
+    for (l; l > 0; l--) {
+      result += str[l-1];
+    }
+    return result;
 }
