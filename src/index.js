1d1dd25a3e494b63d8cfeb9fd9fc5ce7c9a468f1
serhio1973 <anisanis336@gmail.com>
2022-07-05 12:50:52 +0300

    add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+        let str = String(n);
+        let reversStr = str.split("").reverse().join("");
+        n = Number(reversStr);
+        return n;
+    } else {
+        n = (-1)*n;
+        let str = String(n);
+        let reversStr = str.split("").reverse().join("");
+        n = Number(reversStr);
+        return n;
+    };
+
 }
