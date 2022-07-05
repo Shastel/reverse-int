f90e0b360016ceb9fcef17165237e173740e8133
Roman <108676828+Roman-Skor@users.noreply.github.com>
2022-07-05 15:58:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    var str = n.toString();
+    var index;
+	var ret="";
+	for (index = str.length - 1; index >= 0; --index) {
+        ret+=str.charAt(index);
+	}
+    var rez = parseInt(ret);
+	//console.log(rez);
+    return rez;
 }
