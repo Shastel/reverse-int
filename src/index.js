d94b4da8a6b93defc7b84fcd25cf59c72c66043e
romalav <romanlavl@gmail.com>
2022-07-05 19:34:43 +0300

    init:complite reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = Math.abs(n);
+    }  
+    let strNumber = String(n);
+    let i = strNumber.length - 1;
+    let result = '';
+    while (i >= 0) {
+      result = result + strNumber[i];
+      i = i - 1;
+    }
+    let resultNumber = Number(result);
+    return(resultNumber);
 }
