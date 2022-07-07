659f70f7620aed7b058b265c9dcd904329e21e6c
iramiami <iramiami@yandex.ru>
2022-07-07 23:09:16 +0300

    init:end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let reverseString = n;
+    reverseString = n.toString().replace(/[^0-9]/g, '').split('').reverse().join('');
+    return parseInt(reverseString);
+     
 }
