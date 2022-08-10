58236bd15940c40b65aff7d5733421e83b9acc96
AlexSerSla <slavckin2009@yandex.ru>
2022-08-10 16:18:58 +0300

    feat: OK tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let strN = String (Math.abs(n));
+    let reverseStrN = '';
+
+    for (let index = 0; index < strN.length; index++) {
+        reverseStrN = `${strN[index]}${reverseStrN}`;
+    }
+
+    return Number (reverseStrN)
 }
