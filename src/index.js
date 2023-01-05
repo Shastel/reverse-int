9527b0bb1665cb8440617348539cc56787bd57a3
Ow1Max <0w1.go.0w1@gmail.com>
2023-01-05 19:48:48 +0300

    fix: removed bug with negative numbers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
-  const numberToArray = n.toString().split('');
+  const numberToArray = Math.abs(n).toString().split('');
   const result = numberToArray.reverse().join('');
 
   return +result;
