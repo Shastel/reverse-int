cf323ae5e1b21d5de70b9ca4559a8eb815a8bded
Taras <taras.chernov@mail.ru>
2023-01-13 13:18:28 +0300

    "feat: task complete"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n = (n < 0 ? - n : n);
+    let arr = Array.from(String(n));
+    arr.reverse();
+    let reversN = Number(arr.join(''));
+    return (reversN);
 }
