6a0bbe6b25716a9015c39a38d6509f3663679f0a
ZhenyaDorosevich <55046855+ZhenyaDorosevich@users.noreply.github.com>
2023-01-17 18:03:29 +0300

    add:repo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n){
+    const first = Math.abs(n)
+    return  Number(first.toString().split('').reverse().join(''));
 }
