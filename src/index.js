39d9ec283ca61ff559f22b826cfe650674fa898b
Kulerock <1902katerina2011@gmail.com>
2023-07-31 21:08:40 +0300

    add program

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let rev_num = 0;
+    let isNegative = n < 0;
+
+    n = Math.abs(n);
+
+    while (n > 0) {
+        rev_num = rev_num * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+
+    return rev_num;
 }
