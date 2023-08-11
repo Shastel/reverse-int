7ced993351dd16f0dc4b6d45c59e873851597e2e
Orif Sarniyozov <60812094+Orif-92@users.noreply.github.com>
2023-08-11 13:25:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  function reverse(number) {
+  const reversedString = String(number).split('').reverse().join('');
+  return Number(reversedString);
 }
+
+module.exports = reverse;
+
+}
+
+console.log(reverse(123));    
+console.log(reverse(233));    
+console.log(reverse(535));    
+console.log(reverse(95034));  
