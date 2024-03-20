fc00298548cd3a4a07478abe254dd60d7971760b
mahotich <rmahotka@gmail.com>
2024-03-20 11:37:39 +0300

    new reverse

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 module.exports = function reverse(n) {
-    let reversChar = String(n).split("").reverse().join("");
     if (n > 0) {
-        return reversChar;
-    }
-    else {
-        return reversChar.substring(0, reversChar.length - 1);
+        return res = String(n).split('').reverse().join('')
+    } else {
+        return res = String(n).split('').reverse().join('').replace('-', '')
     }
 }
